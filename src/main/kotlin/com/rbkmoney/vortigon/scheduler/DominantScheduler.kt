dae3a8cf42b1b48f37045eab58c83a71b5760f43
vitaxa <vitaxa93gamebox@gmail.com>
2021-09-14 15:43:46 +0300

    Fix scheduler config (#6)

--- src/main/kotlin/com/rbkmoney/vortigon/scheduler/DominantScheduler.kt
+++ src/main/kotlin/com/rbkmoney/vortigon/scheduler/DominantScheduler.kt
@@ -12,13 +12,13 @@ import org.springframework.stereotype.Service
 private val log = KotlinLogging.logger {}
 
 @Service
-@ConditionalOnProperty(prefix = "service", name = ["dominant.scheduler.enabled"], havingValue = "true")
+@ConditionalOnProperty(prefix = "scheduler", name = ["enabled"], havingValue = "true")
 class DominantScheduler(
     private val dominantService: DominantService,
-    @Value("\${service.dominant.scheduler.querySize:0}") private val querySize: Int
+    @Value("\${scheduler.querySize:0}") private val querySize: Int
 ) {
 
-    @Scheduled(fixedDelayString = "\${service.dominant.scheduler.pollingDelay}")
+    @Scheduled(fixedDelayString = "\${scheduler.pollingDelay}")
     @SchedulerLock(name = "dominantPullTask")
     fun pollScheduler() {
         try {
